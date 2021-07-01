83b067f1d5eed76afc945a95aa0c55867c95a861
Rohit Malik <bpsrohitmalik@gmail.com>
2021-07-01 22:16:01 +0530

    Add project files.

---
+++ ebook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ebook.Startup))]
+namespace ebook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
